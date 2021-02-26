247711212fa55930dc3df8bdeaea11b9a4116d36
sizolla <iamjuwon@gmail.com>
2021-02-26 10:27:37 +0900

    Create RosouLexiuCirculationFan.js

---
+++ Devices/RosouLexiuCirculationFan.js
@@ -0,0 +1,88 @@
+require('./Base');
+
+const inherits = require('util').inherits;
+const miio = require('miio');
+
+var Accessory, PlatformAccessory, Service, Characteristic, UUIDGen;
+
+DmakerFan = function(platform, config) {
+    this.init(platform, config);
+    
+    Accessory = platform.Accessory;
+    PlatformAccessory = platform.PlatformAccessory;
+    Service = platform.Service;
+    Characteristic = platform.Characteristic;
+    UUIDGen = platform.UUIDGen;
+    
+    this.device = new miio.Device({
+        address: this.config['ip'],
+        token: this.config['token']
+    });
+    
+    this.accessories = {};
+    if(!this.config['fanDisable'] && this.config['fanName'] && this.config['fanName'] != "") {
+        this.accessories['fanAccessory'] = new RosouLexiuCirculationFanAccessory(this);
+    }
+	
+    if(!this.config['buzzerSwitchDisable'] && this.config['buzzerSwitchName'] && this.config['buzzerSwitchName'] != "") {
+        this.accessories['buzzerSwitchAccessory'] = new RosouLexiuCirculationFanBuzzerSwitchAccessory(this);
+    }
+	
+    var accessoriesArr = this.obj2array(this.accessories);
+    
+    this.platform.log.debug("[MiFanPlatform][DEBUG]Initializing " + this.config["type"] + " device: " + this.config["ip"] + ", accessories size: " + accessoriesArr.length);
+    
+    return accessoriesArr;
+}
+inherits(RosouLexiuCirculationFan, Base);
+
+RosouLexiuCirculationFanAccessory = function(dThis) {
+    this.device = dThis.device;
+    this.name = dThis.config['fanName'];
+    this.platform = dThis.platform;
+}
+
+RosouLexiuCirculationFanAccessory.prototype.getServices = function() {
+    var that = this;
+    var services = [];
+
+    var infoService = new Service.AccessoryInformation();
+    infoService
+        .setCharacteristic(Characteristic.Manufacturer, "XiaoMi")
+        .setCharacteristic(Characteristic.Model, "RosouLexiuCirculationFan")
+        .setCharacteristic(Characteristic.SerialNumber, "Undefined");
+    services.push(infoService);
+
+    var fanService = new Service.Fanv2(this.name);
+    var activeCharacteristic = fanService.getCharacteristic(Characteristic.Active);
+
+    //power on/off
+    activeCharacteristic
+        .on('get', function(callback) {
+            that.device.call("get_prop", ["power"]).then(result => {
+                that.platform.log.debug("[MiFanPlatform][DEBUG]MiDCVFFanFanAccessory - Active - getActive: " + result);
+                callback(null, result[0] === "0" ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE);
+            }).catch(function(err) {
+                that.platform.log.error("[MiFanPlatform][ERROR]MiDCVFFanFanAccessory - Active - getActive Error: " + err);
+                callback(err);
+            });
+        }.bind(this))
+        .on('set', function(value, callback) {
+            that.platform.log.debug("[MiFanPlatform][DEBUG]MiDCVFFanFanAccessory - Active - setActive: " + value);
+            that.device.call("set_power", [value ? "1" : "0"]).then(result => {
+                that.platform.log.debug("[MiFanPlatform][DEBUG]MiDCVFFanFanAccessory - Active - setActive Result: " + result);
+                if(result[0] === "ok") {
+                    callback(null);
+                } else {
+                    callback(new Error(result[0]));
+                }            
+            }).catch(function(err) {
+                that.platform.log.error("[MiFanPlatform][ERROR]MiDCVFFanFanAccessory - Active - setActive Error: " + err);
+                callback(err);
+            });
+        }.bind(this));
+    
+    services.push(fanService);
+
+    return services;
+}

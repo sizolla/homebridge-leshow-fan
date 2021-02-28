9b0af2512ab9e6c90112c2744f36c002047917be
sizolla <iamjuwon@gmail.com>
2021-03-01 00:09:31 +0900

    Update RosouLexiuCirculationFan.js

--- Devices/RosouLexiuCirculationFan.js
+++ Devices/RosouLexiuCirculationFan.js
@@ -51,33 +51,63 @@ RosouLexiuCirculationFanAccessory.prototype.getServices = function() {
 
     var fanService = new Service.Fanv2(this.name);
     var activeCharacteristic = fanService.getCharacteristic(Characteristic.Active);
+    var swingModeCharacteristic = fanService.addCharacteristic(Characteristic.SwingMode);
 
     //power on/off
     activeCharacteristic
         .on('get', function(callback) {
             that.device.call("get_prop", ["power"]).then(result => {
                 that.platform.log.debug("[MiFanPlatform][DEBUG]FanAccessory - Active - getActive: " + result);
-                callback(null, result[0] === "0" ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE);
+                callback(null, result == 1 ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE);
             }).catch(function(err) {
-                that.platform.log.error("[MiFanPlatform][ERROR]FanFanAccessory - Active - getActive Error: " + err);
+                that.platform.log.error("[MiFanPlatform][ERROR]FanAccessory - Active - getActive Error: " + err);
                 callback(err);
             });
         }.bind(this))
         .on('set', function(value, callback) {
-            that.platform.log.debug("[MiFanPlatform][DEBUG]FanFanAccessory - Active - setActive: " + value);
-            that.device.call("set_power", [value ? "1" : "0"]).then(result => {
-                that.platform.log.debug("[MiFanPlatform][DEBUG]FanFanAccessory - Active - setActive Result: " + result);
-                if(result[0] === "ok") {
+            that.platform.log.debug("[MiFanPlatform][DEBUG]FanAccessory - Active - setActive: " + value);
+            that.device.call("set_power", [value ? 1 : 0]).then(result => {
+                that.platform.log.debug("[MiFanPlatform][DEBUG]FanAccessory - Active - setActive Result: " + result);
+                if(result == "ok") {
                     callback(null);
                 } else {
-                    callback(new Error(result[0]));
+                    callback(new Error(result));
                 }            
             }).catch(function(err) {
-                that.platform.log.error("[MiFanPlatform][ERROR]FanFanAccessory - Active - setActive Error: " + err);
+                that.platform.log.error("[MiFanPlatform][ERROR]FanAccessory - Active - setActive Error: " + err);
                 callback(err);
             });
         }.bind(this));
-    
+
+    swingModeCharacteristic
+  	    .on('get', function(callback) {
+	        that.device.call("get_prop", ["yaw"]).then(result => {
+	            that.platform.log.debug("[MiFanPlatform][DEBUG]FanAccessory - SwingMode - getSwingModeControls: " + result);
+		if (result == 3) {
+		    callback(null, Characteristic.SwingMode.SWING_ENABLED);
+		} else {
+		    callback(null, Characteristic.SwingMode.SWING_DISABLED);
+		}
+	    }).catch(function(err) {
+	        that.platform.log.error("[MiFanPlatform][ERROR]FanAccessory - SwingMode - getSwingModeControls Error: " + err);
+                callback(err);
+            });
+        }.bind(this))
+        .on('set', function(value, callback) {	
+            that.platform.log.debug("[MiFanPlatform][DEBUG]FanAccessory - SwingMode - setSwingModeControls: " + value);
+	            that.device.call("set_yaw", [value ? 3 : 0]).then(result => {
+		    that.platform.log.debug("[MiFanPlatform][DEBUG]FanAccessory - SwingMode - setSwingModeControls Result: " + result);
+                if(result == "ok") {
+                    callback(null);
+                } else {
+                     callback(new Error(result[0]));
+                }
+	     }).catch(function(err) {
+	       that.platform.log.error("[MiFanPlatform][ERROR]FanAccessory - SwingMode - setSwingModeControls Error: " + err);
+                callback(err);
+	     });
+        }.bind(this));
+
     services.push(fanService);
 
     return services;
